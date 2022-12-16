f10b6a1586f8edaa0d40804d2771825e6c0376db
ReynaRamirez-Arellano <1017annabeth@gmail.com>
2022-12-16 20:50:13 +0900

    HW

---
+++ hayul/python-HW/Day1-HW.py
@@ -0,0 +1,12 @@
+# j=1
+# print(i+j)
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(orange%apple)
+print(orange//apple)
+print(2**2)
+print(3**3)
