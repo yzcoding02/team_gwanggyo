a7851972ddce9611aba7f3a51a9f88b1104b8e8b
K-Haseul <yeounkkyugwi@gmail.com>
2022-12-16 20:47:40 +0900

    숙제제

---
+++ seohyun/python_HW/Day1_HW.py
@@ -0,0 +1,19 @@
+# print("안녕 파이썬")
+# i=0
+# print(i)
+# j=1
+# print(i+j)
+
+# print(k)
+
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(orange%apple)
+print(orange//apple)
+
+print(2**2)
+print(3**3)
