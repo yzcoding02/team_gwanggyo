eec014e90d2a0b1c99cb2ae03f92f4080c7c717f
K-Haseul <yeounkkyugwi@gmail.com>
2023-01-13 17:53:05 +0900

    숙제제

---
+++ seohyun/python_HW/Day3_HW.py
@@ -0,0 +1,61 @@
+#1
+for i in range(1,11):
+    print(i)
+
+i=1
+while i<11:
+    print(i)
+    i+=1
+
+#2
+for i in range(2,11,2):
+  if i%2==0:
+   print(i)
+
+i=1
+while i<11:
+    if i%2==0:
+        print(i)
+
+#3
+sum=0
+for i in range(1,11):
+  if i%3==0:
+    sum+=i
+print(sum)
+
+sum=0
+i=1
+while i<11:
+    if i%3==0:
+        sum+=i
+    i+=1
+print(sum)
+
+#4
+for i in range(50,101):
+  if i%3==0:
+    if i%7==0:
+        print(i)
+
+i=50
+while i<101:
+    if i%3==0:
+        if i%7==0:
+            print(i)
+    i+=1
+
+#5
+sum=1
+for i in range(1,21):
+  if i%2==0:
+    sum*=i
+print(sum)
+
+i=1
+sum=1
+while i<21:
+    if i%2==0:
+        sum*=i
+    i+=1
+print(sum)
