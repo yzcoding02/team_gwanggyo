7fef0c02b65d98d903580103ed9074c3d9badc35
ReynaRamirez-Arellano <1017annabeth@gmail.com>
2023-02-10 19:42:52 +0900

    숙제

---
+++ hayul/python-HW/Day4 hw.py
@@ -0,0 +1,17 @@
+# for i in range(10,1,-1):
+#     print(i)
+# def 최소공배수(a,b):
+#     if (a>b):
+#         for i in range(b,1,-1):
+#             if a%i==0:
+#                 if b%i==0:
+#                     print(i)
+#     else:
+#         for i in range(a,1,-1):
+#             if a%i==0:
+#                 if b%i==0:
+#                     print(i)
+# print (최소공배수(10,12))
+# 결과=최소공배수(6,4)
+# print(결과)
+
