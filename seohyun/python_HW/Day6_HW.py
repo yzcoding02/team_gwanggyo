bf43a35c281a90674db39a0558d47b28469291e9
K-Haseul <yeounkkyugwi@gmail.com>
2023-02-10 19:39:12 +0900

    짝수

--- seohyun/python_HW/Day6_HW.py
+++ seohyun/python_HW/Day6_HW.py
@@ -1,9 +1,10 @@
 def 짝수(a):
     list=[]
-    for i in range(a*2):
+    for i in range(2,a*2+1):
         if i%2==0:
             list.append(i)
             list.sort()
             list.reverse()
     print(list)
-짝수(9)
+짝수(3)
+짝수(6)
