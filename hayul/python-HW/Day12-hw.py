3347a4e4e86a0d141fecdf30e30c9cfe966b1a45
ReynaRamirez-Arellano <1017annabeth@gmail.com>
2023-04-28 16:31:16 +0900

    homework

--- hayul/python-HW/Day12-hw.py
+++ hayul/python-HW/Day12-hw.py
@@ -1,4 +1,4 @@
 for i in range(3):
     num1=input("첫 번쨰 숫자를 입력해주세요:")
     num2=input("두 번째 숫자를 입력해주세요:")
-    print(f"두 수의 합은 {int(num1)+int(num2)}이고, 두 수의 차은 {int(num1)-int(num2)}이고, 두 수의 곱은 {int(num1)*int(num2)}이고, 두 수의 몫은 {int(num1)//int(num2)}이고, 두 수의 나머지는 {int(num1)%int(num2)}입니다")
+    print(f"먼저 두 수의 합은 {int(num1)+int(num2)}이고, 두 수의 차은 {int(num1)-int(num2)}이고, 두 수의 곱은 {int(num1)*int(num2)}이고, 두 수의 몫은 {int(num1)//int(num2)}이고,마지막으로, 두 수의 나머지는 {int(num1)%int(num2)}입니다")
